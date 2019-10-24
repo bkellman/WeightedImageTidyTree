382a4fd150f9dc290ade43fc9c45fae713bfb9ff
TylerReaganUCSD <tareagan@ucsd.edu>
2019-10-23 21:41:12 -0700

    making a new branch for the zoomable tree

--- WorkingTidy/README.md
+++ WorkingTidy/README.md
@@ -1,6 +1,6 @@
-# Tidy Tree
+# Collapsible Tree
 
-https://observablehq.com/d/2944ddfd576b076f@132
+https://observablehq.com/d/3db20e78745501fb@347
 
 View this notebook in your browser by running a web server in this folder. For
 example:
@@ -14,14 +14,14 @@ import this module directly into your application. To npm install:
 
 ~~~sh
 npm install @observablehq/runtime@4
-npm install https://api.observablehq.com/d/2944ddfd576b076f.tgz?v=3
+npm install https://api.observablehq.com/d/3db20e78745501fb.tgz?v=3
 ~~~
 
 Then, import your notebook and the runtime as:
 
 ~~~js
 import {Runtime, Inspector} from "@observablehq/runtime";
-import define from "2944ddfd576b076f";
+import define from "3db20e78745501fb";
 ~~~
 
 To log the value of the cell named “foo”:
--- WorkingTidy/d3/main.js
+++ WorkingTidy/d3/main.js
@@ -1,94 +1,171 @@
-// https://observablehq.com/d/2944ddfd576b076f@132
+// https://observablehq.com/d/3db20e78745501fb@347
 export default function define(runtime, observer) {
   const main = runtime.module();
+// This block of code is responsible for appending a section of markup at the top of the page
+//   main.variable(observer()).define(["md"], function(md){return(
+// md`# Collapsible Tree
 
-  main.variable(observer("chart")).define("chart", ["tree","data","d3","width"], function(tree,data,d3,width)
+// Click a black node to expand or collapse [the tree](/@d3/tidy-tree).`
+// )});
+  main.variable(observer("chart")).define("chart", ["d3","data","dy","margin","width","dx","tree","diagonal"], function(d3,data,dy,margin,width,dx,tree,diagonal)
 {
-  const root = tree(data);
+  const root = d3.hierarchy(data);
 
-  let x0 = Infinity;
-  let x1 = -x0;
-  root.each(d => {
-    if (d.x > x1) x1 = d.x;
-    if (d.x < x0) x0 = d.x;
+  root.x0 = dy / 2;
+  root.y0 = 0;
+  root.descendants().forEach((d, i) => {
+    d.id = i;
+    d._children = d.children;
+    if (d.depth && d.data.name.length !== 7) d.children = null;
   });
 
   const svg = d3.create("svg")
-      .attr("viewBox", [0, 0, width, x1 - x0 + root.dx * 2]);
-  
-  const g = svg.append("g")
-      .attr("font-family", "sans-serif")
-      .attr("font-size", 10)
-      .attr("transform", `translate(${root.dy / 3},${root.dx - x0})`);
-    
-  const link = g.append("g")
-    .attr("fill", "none")
-    .attr("stroke", "#555")
-    .attr("stroke-opacity", 0.4)
-    .attr("stroke-width", 1.5)
-  .selectAll("path")
-    .data(root.links())
-    .join("path")
-      .attr("d", d3.linkHorizontal()
-          .x(d => d.y)
-          .y(d => d.x));
-  
-  const node = g.append("g")
-      .attr("stroke-linejoin", "round")
-      //controls the width of the border surrounging the nodes
-      .attr("stroke-width", 1)
-      .selectAll("g")
-      .data(root.descendants())
-      .join("g")
-      .attr("transform", d => `translate(${d.y},${d.x})`);
+      .attr("viewBox", [-margin.left, -margin.top, width, dx])
+      .style("font", "10px sans-serif")
+      .style("user-select", "none");
 
-  node.append("circle")
-      .attr("fill", d => d.children ? "#555" : "#999")
-      .attr("r", 3.5);
+  const gLink = svg.append("g")
+      .attr("fill", "none")
+      .attr("stroke", "#555")
+      .attr("stroke-opacity", 0.4)
+      .attr("stroke-width", 1.5);
 
-      // this will size the nodes by value
-      /* 
-      .attr("r", function (d) { return d.data.size * 2.5; })
-      */
+  const gNode = svg.append("g")
+      .attr("cursor", "pointer")
+      .attr("pointer-events", "all");
 
-      // this will color the border of the nodes by value
-      /*
-      .style("stroke", "red")
-      */
+  function update(source) {
+    const duration = d3.event && d3.event.altKey ? 2500 : 250;
+    const nodes = root.descendants().reverse();
+    const links = root.links();
 
-  node.append("text")
-      .attr("dy", "0.31em")
-      .attr("x", d => d.children ? -6 : 6)
-      .attr("text-anchor", d => d.children ? "end" : "start")
-      .text(d => d.data.name)
-    .clone(true).lower()
-      .attr("stroke", "white");
-    
-  // node.append("image")
-  //     .attr("xlink:href", function (d) { return d.data.img; })
-  //     .attr("x", function (d) { return -6; })
-  //     .attr("y", function (d) { return -6; })
-  //     .attr("height", 12)
-  //     .attr("width", 12);
-  
+    // Compute the new tree layout.
+    tree(root);
+
+    let left = root;
+    let right = root;
+    root.eachBefore(node => {
+      if (node.x < left.x) left = node;
+      if (node.x > right.x) right = node;
+    });
+
+    const height = right.x - left.x + margin.top + margin.bottom;
+
+    const transition = svg.transition()
+        .duration(duration)
+        .attr("viewBox", [-margin.left, left.x - margin.top, width, height])
+        .tween("resize", window.ResizeObserver ? null : () => () => svg.dispatch("toggle"));
+
+    // Update the nodes…
+    const node = gNode.selectAll("g")
+      .data(nodes, d => d.id);
+
+    // Enter any new nodes at the parent's previous position.
+    const nodeEnter = node.enter().append("g")
+        .attr("transform", d => `translate(${source.y0},${source.x0})`)
+        .attr("fill-opacity", 0)
+        .attr("stroke-opacity", 0)
+        .on("click", d => {
+          d.children = d.children ? null : d._children;
+          update(d);
+        });
+
+    nodeEnter.append("circle")
+        .attr("r", 2.5)
+        .attr("fill", d => d._children ? "#555" : "#999")
+        .attr("stroke-width", 10);
+
+        // this will size the nodes by value
+        /* 
+        .attr("r", function (d) { return d.data.size * 2.5; })
+        */
+
+        // this will color the border of the nodes by value
+        /*
+        .style("stroke", "red")
+        */
+
+    nodeEnter.append("text")
+        .attr("dy", "0.31em")
+        .attr("x", d => d._children ? -6 : 6)
+        .attr("text-anchor", d => d._children ? "end" : "start")
+        .text(d => d.data.name)
+      .clone(true).lower()
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-width", 3)
+        .attr("stroke", "white");
+
+    // nodeEnter.append("image")
+    //     .attr("xlink:href", function (d) { return d.data.img; })
+    //     .attr("x", function (d) { return -6; })
+    //     .attr("y", function (d) { return -6; })
+    //     .attr("height", 12)
+    //     .attr("width", 12);
+
+    // Transition nodes to their new position.
+    const nodeUpdate = node.merge(nodeEnter).transition(transition)
+        .attr("transform", d => `translate(${d.y},${d.x})`)
+        .attr("fill-opacity", 1)
+        .attr("stroke-opacity", 1);
+
+    // Transition exiting nodes to the parent's new position.
+    const nodeExit = node.exit().transition(transition).remove()
+        .attr("transform", d => `translate(${source.y},${source.x})`)
+        .attr("fill-opacity", 0)
+        .attr("stroke-opacity", 0);
+
+    // Update the links…
+    const link = gLink.selectAll("path")
+      .data(links, d => d.target.id);
+
+    // Enter any new links at the parent's previous position.
+    const linkEnter = link.enter().append("path")
+        .attr("d", d => {
+          const o = {x: source.x0, y: source.y0};
+          return diagonal({source: o, target: o});
+        });
+
+    // Transition links to their new position.
+    link.merge(linkEnter).transition(transition)
+        .attr("d", diagonal);
+
+    // Transition exiting nodes to the parent's new position.
+    link.exit().transition(transition).remove()
+        .attr("d", d => {
+          const o = {x: source.x, y: source.y};
+          return diagonal({source: o, target: o});
+        });
+
+    // Stash the old positions for transition.
+    root.eachBefore(d => {
+      d.x0 = d.x;
+      d.y0 = d.y;
+    });
+  }
+
+  update(root);
+
   return svg.node();
 }
 );
+  main.variable(observer("diagonal")).define("diagonal", ["d3"], function(d3){return(
+d3.linkHorizontal().x(d => d.y).y(d => d.x)
+)});
+  main.variable(observer("tree")).define("tree", ["d3","dx","dy"], function(d3,dx,dy){return(
+d3.tree().nodeSize([dx, dy])
+)});
   main.variable(observer("data")).define("data", ["d3"], function(d3){return(
-    //d3.json("https://gist.githubusercontent.com/TylerReaganUCSD/6f072c82cf29ce3d874b2e36a2486848/raw/cf8b0bda4e6418635bd4f8816d460a55042bd334/benTree.json")
-    //d3.json("https://gist.githubusercontent.com/TylerReaganUCSD/cc196475a316d308a91b88818ade41d0/raw/24536288add67217e0887c8f5cf042f31f5b1981/imageTest.json")
-    d3.json("https://raw.githubusercontent.com/bkellman/WeightedImageTidyTree/master/BenTree%20Converter/hierarchy.json")
+// d3.json("https://raw.githubusercontent.com/d3/d3-hierarchy/v1.1.8/test/data/flare.json")
+d3.json("https://raw.githubusercontent.com/bkellman/WeightedImageTidyTree/master/BenTree%20Converter/hierarchy.json")
 )});
-  main.variable(observer("tree")).define("tree", ["d3","width"], function(d3,width){return(
-data => {
-  const root = d3.hierarchy(data);
-  root.dx = 10;
-  root.dy = width / (root.height + 1);
-  return d3.tree().nodeSize([root.dx, root.dy])(root);
-}
+  main.variable(observer("dx")).define("dx", function(){return(
+10
 )});
-  main.variable(observer("width")).define("width", function(){return(
-932
+  main.variable(observer("dy")).define("dy", ["width"], function(width){return(
+width / 6
+)});
+  main.variable(observer("margin")).define("margin", function(){return(
+{top: 10, right: 120, bottom: 10, left: 40}
 )});
   main.variable(observer("d3")).define("d3", ["require"], function(require){return(
 require("d3@5")
